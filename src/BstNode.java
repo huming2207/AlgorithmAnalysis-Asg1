766dc3c9f8d6b6445ebd16006b61bde31dd6c4db
Jackson Ming Hu <huming2207@gmail.com>
2017-08-11 11:18:02 +1000

    BstNode: Improve format and quality

--- src/BstNode.java
+++ src/BstNode.java
@@ -1,47 +1,66 @@
 //Node for Binary Search Tree
-public class BstNode<T extends Comparable<T>> {
-	//Left Node in BST
-	 private BstNode<T> LeftNode;
-	 //Right Node in BST
-	 private BstNode<T> RightNode;
-	 //element stored in the Node
-	 private T item;
-	 //record the number of instance
-	 private int instanceCount;
- 	
-	 public BstNode(T item, int instanceCount, BstNode<T> LeftNode, BstNode<T> RightNode)
-	 {
-		 this.setLeftNode(LeftNode);
-		 this.setRightNode(RightNode);
-		 this.item = item;
-		 this.instanceCount = instanceCount;	
-	 }
-	 
-	 public BstNode(T item) {this.item = item;}
-	 
-	 public BstNode<T> getLeftNode(){
-		 return LeftNode;
-	 }
-	 public void setLeftNode(BstNode<T> leftNode) {
-		 LeftNode = leftNode;
-	 }
-	 public BstNode<T> getRightNode(){
-		 return RightNode;
-	 }
-	 public void setRightNode(BstNode<T> rightNode) {
-		 RightNode = rightNode;
-	 }
-	 public T getItme(){
-		 return item;
-	 }
-	 public void setItem(T item) {
-		 this.item = item;
-	 }
-	 public int getInstanceCount() {
-		 return instanceCount;	 
-	 }
-	 public void setInstanceCount(int instanceCount) {
-		 this.instanceCount = instanceCount;
-	 }
-	 
+public class BstNode<T extends Comparable<T>>
+{
+    //Left Node in BST
+    private BstNode<T> LeftNode;
+
+    //Right Node in BST
+    private BstNode<T> RightNode;
+
+    //element stored in the Node
+    private T item;
+
+    //record the number of instance
+    private int instanceCount;
+
+    public BstNode(T item, int instanceCount, BstNode<T> LeftNode, BstNode<T> RightNode)
+    {
+        this.setLeftNode(LeftNode);
+        this.setRightNode(RightNode);
+        this.item = item;
+        this.instanceCount = instanceCount;
+    }
+
+    public BstNode(T item) {this.item = item;}
+
+    public BstNode<T> getLeftNode()
+    {
+        return LeftNode;
+    }
+
+    public void setLeftNode(BstNode<T> leftNode)
+    {
+        LeftNode = leftNode;
+    }
+
+    public BstNode<T> getRightNode()
+    {
+        return RightNode;
+    }
+
+    public void setRightNode(BstNode<T> rightNode)
+    {
+        RightNode = rightNode;
+    }
+
+    public T getItme()
+    {
+        return item;
+    }
+
+    public void setItem(T item)
+    {
+        this.item = item;
+    }
+
+    public int getInstanceCount()
+    {
+        return instanceCount;
+    }
+
+    public void setInstanceCount(int instanceCount)
+    {
+        this.instanceCount = instanceCount;
+    }
+
 }
