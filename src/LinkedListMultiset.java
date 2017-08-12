3dbefc2228aa7d9ce7140ee1b63176d27989658a
Jackson Ming Hu <huming2207@gmail.com>
2017-08-12 22:45:41 +1000

    LinkedList: reformat code

--- src/LinkedListMultiset.java
+++ src/LinkedListMultiset.java
@@ -2,155 +2,154 @@ import java.io.PrintStream;
 
 public class LinkedListMultiset<T extends Comparable<T>> extends Multiset<T>
 {
-	// The first node (HEAD)
-	private Node<T> firstNode;
+    // The first node (HEAD)
+    private Node<T> firstNode;
 
-	// The last node (TAIL)
-	private Node<T> lastNode;
+    // The last node (TAIL)
+    private Node<T> lastNode;
 
-	private int countNode;
+    private int countNode;
 
-	public LinkedListMultiset() {
-		// Implement me!
-		firstNode = null;
-		lastNode = null;
-		countNode = 0;
-	} // end of LinkedListMultiset()
-
-
-	public void add(T item) {
-		// Implement me!
-		//create a new node
-		Node<T> newNode = new Node<>(item);
-		newNode.setInstanceCount(updateInstanceCount(item, 1));
-
-		if(firstNode == null)
-		{
-			// Here we don't need to do any search, since the first node is null (whole list is empty)
-			firstNode = newNode;
-			lastNode = newNode;
-		}
-		else
-		{
-			// Run a search before addition
-			Node<T> searchResult = searchHelper(item);
-
-			if(searchResult != null)
-			{
-				// If there is a node already exists with the same item, just +1 for instance count.
-				searchResult.setInstanceCount(searchResult.getInstanceCount() + 1);
-			}
-			else
-			{
-				// ...otherwise, add the item to a new node
-				newNode.setPreviousNode(lastNode);
-				lastNode.setNextNode(newNode);
-				lastNode = newNode;
-			}
-
-		}
-		countNode++;
-	} // end of add()
-
-	private int updateInstanceCount(T item, int update)
-	{
-		Node<T> nodePointer = firstNode;
-		int count = 1;
-		while(nodePointer != null)
-		{
-			if(nodePointer.getItem().equals(item))
-			{
-				nodePointer.setInstanceCount(nodePointer.getInstanceCount()+update);
-				count = nodePointer.getInstanceCount();
-			}
-		}
-		return count;
-	}
+    public LinkedListMultiset()
+    {
+        firstNode = null;
+        lastNode = null;
+        countNode = 0;
+    } // end of LinkedListMultiset()
 
 
-	public int search(T item) {
-		// Implement me!
-		Node<T> nodePointer = searchHelper(item);
-		// default return, please override when you implement this method
-		return (nodePointer!=null)? nodePointer.getInstanceCount():0;
-	} // end of search()
+    public void add(T item)
+    {
+        // Create a new node
+        Node<T> newNode = new Node<>(item);
+        newNode.setInstanceCount(updateInstanceCount(item, 1));
 
-	private Node<T> searchHelper(T item)
-	{
-		Node<T> nodePointer = firstNode;
-		while(nodePointer != null)
-		{
-			if(nodePointer.getItem().equals(item))
-			{
-				return nodePointer;
-			}
+        if (firstNode == null)
+        {
+            // Here we don't need to do any search, since the first node is null (whole list is empty)
+            firstNode = newNode;
+            lastNode = newNode;
+        }
+        else
+        {
+            // Run a search before addition
+            Node<T> searchResult = searchHelper(item);
 
-			nodePointer = nodePointer.getNextNode();
-		}
-		return null;
-	}
+            if (searchResult != null)
+            {
+                // If there is a node already exists with the same item, just +1 for instance count.
+                searchResult.setInstanceCount(searchResult.getInstanceCount() + 1);
+            }
+            else
+            {
+                // ...otherwise, add the item to a new node
+                newNode.setPreviousNode(lastNode);
+                lastNode.setNextNode(newNode);
+                lastNode = newNode;
+            }
 
-	private void removeNode(Node<T> node)
-	{
-		if(firstNode.equals(node) && lastNode.equals(node))
-		{
-			firstNode = null;
-			lastNode = null;
-		}
-		if(node.getPreviousNode() != null)
-		{
-			node.getPreviousNode().setNextNode(node.getNextNode());
-		}
-		else
-		{
-			firstNode = node.getNextNode();
-		}
-		if(node.getNextNode() != null)
-		{
-			node.getNextNode().setPreviousNode(node.getPreviousNode());
-		}
-		else
-		{
-			lastNode = node.getPreviousNode();
-		}
-		node.setItem(null);
-		countNode --;
-	}
+        }
+        countNode++;
+    } // end of add()
 
-	public void removeOne(T item) {
-		// Implement me!
-		Node<T> node = searchHelper(item);
-
-		removeNode(node);
-		updateInstanceCount(item,-1);
-
-	} // end of removeOne()
+    private int updateInstanceCount(T item, int update)
+    {
+        Node<T> nodePointer = firstNode;
+        int count = 1;
+        while (nodePointer != null)
+        {
+            if (nodePointer.getItem().equals(item))
+            {
+                nodePointer.setInstanceCount(nodePointer.getInstanceCount() + update);
+                count = nodePointer.getInstanceCount();
+            }
+        }
+        return count;
+    }
 
 
-	public void removeAll(T item) {
-		// Implement me!
-		Node<T> nodePointer = firstNode;
-		while(nodePointer != null)
-		{
-			if(nodePointer.getItem().equals(item))
-			{
-				removeNode(nodePointer);
-			}
-			nodePointer = nodePointer.getNextNode();
-		}
-	} // end of removeAll()
+    public int search(T item)
+    {
+        Node<T> nodePointer = searchHelper(item);
+        return (nodePointer != null) ? nodePointer.getInstanceCount() : 0;
+    } // end of search()
+
+    private Node<T> searchHelper(T item)
+    {
+        Node<T> nodePointer = firstNode;
+        while (nodePointer != null)
+        {
+            if (nodePointer.getItem().equals(item))
+            {
+                return nodePointer;
+            }
+
+            nodePointer = nodePointer.getNextNode();
+        }
+        return null;
+    }
+
+    private void removeNode(Node<T> node)
+    {
+        if (firstNode.equals(node) && lastNode.equals(node))
+        {
+            firstNode = null;
+            lastNode = null;
+        }
+        if (node.getPreviousNode() != null)
+        {
+            node.getPreviousNode().setNextNode(node.getNextNode());
+        }
+        else
+        {
+            firstNode = node.getNextNode();
+        }
+        if (node.getNextNode() != null)
+        {
+            node.getNextNode().setPreviousNode(node.getPreviousNode());
+        }
+        else
+        {
+            lastNode = node.getPreviousNode();
+        }
+        node.setItem(null);
+        countNode--;
+    }
+
+    public void removeOne(T item)
+    {
+        // Implement me!
+        Node<T> node = searchHelper(item);
+
+        removeNode(node);
+        updateInstanceCount(item, -1);
+
+    } // end of removeOne()
 
 
+    public void removeAll(T item)
+    {
+        Node<T> nodePointer = firstNode;
+        while (nodePointer != null)
+        {
+            if (nodePointer.getItem().equals(item))
+            {
+                removeNode(nodePointer);
+            }
+            nodePointer = nodePointer.getNextNode();
+        }
+    } // end of removeAll()
 
-	public void print(PrintStream out) {
-		// Implement me!
-		Node<T> nodePointer = firstNode;
-		while(nodePointer != null)
-		{
-			out.println(nodePointer.getItem()+printDelim+ nodePointer.getInstanceCount());
-			nodePointer = nodePointer.getNextNode();
-		}
-	} // end of print()
+
+    public void print(PrintStream out)
+    {
+        Node<T> nodePointer = firstNode;
+        while (nodePointer != null)
+        {
+            out.println(nodePointer.getItem() + printDelim + nodePointer.getInstanceCount());
+            nodePointer = nodePointer.getNextNode();
+        }
+    } // end of print()
 
 } // end of class LinkedListMultiset
 
